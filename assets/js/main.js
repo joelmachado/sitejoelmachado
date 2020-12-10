35a24a030ce24ad5511ab34b11e1d8313924e3af
imagistica <contas@imagistica.com.br>
2020-12-10 17:00:01 -0300

    tel

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,14 +19,14 @@ popupWhatsApp = () => {
 	
 	btnOpenPopup.addEventListener("click",  () => {
 	  popup.classList.toggle('is-active-whatsapp-popup')
-	   popup.style.animation = "fadeIn .6s 0.0s both";
+	   popup.style.animation = "fadeIn .3s 0.0s both";
 	})
 	
 	sendBtn.addEventListener("click", () => {
 	let msg = document.getElementById('whats-in').value;
 	let relmsg = msg.replace(/ /g,"%20");
 	  //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
-	 window.open('https://wa.me/15551234567?text='+relmsg, '_blank'); 
+	 window.open('https://wa.me/5545998003820?text='+relmsg, '_blank'); 
 	
 	});
   
